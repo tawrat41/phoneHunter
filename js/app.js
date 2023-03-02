305a0fc2a371af8d6c90489121266907431deceb
tawrat41 <tawratur.rashid@gmail.com>
2023-03-02 18:47:38 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,81 @@
+ const loadPhones = async(searchText, dataLimit) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data, dataLimit);
+ }
+
+
+ const displayPhones = (phones, dataLimit) =>{
+    const phoneConatainer = document.getElementById('phone-container')
+    phoneConatainer.innerText = '';
+
+    // no phone message
+    const noPhone = document.getElementById('no-phone-message');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none');
+    }
+    else{
+        noPhone.classList.add('d-none'); 
+    }
+
+    // for displaying 20 phone
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length>10){
+        phones = phones.slice(0,10);
+        showAll.classList.remove('d-none');
+    }
+    else{
+        showAll.classList.add('d-none');
+    }
+
+
+    // display
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card" style="width: 18rem;"> 
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the
+                    bulk of
+                    the card's content.</p>
+            </div>
+        </div>
+        `
+
+        phoneConatainer.appendChild(phoneDiv); 
+    });
+    // stop spinner
+    toggleSpinner(false);
+ }
+
+ const processSearch = (dataLimit) =>{
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit);
+ }
+
+ document.getElementById('btn-search').addEventListener('click',function(){
+    // spin start
+    processSearch(10);
+ })
+
+ const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+ }
+
+ document.getElementById('btn-show-all').addEventListener('click', function(){
+    processSearch();
+ })
+
+ loadPhones();
